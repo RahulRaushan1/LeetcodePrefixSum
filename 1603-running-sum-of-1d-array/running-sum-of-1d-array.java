e686e87e72d41779d08f2e1d0bc2a10f351af97c
Rahul Raushan <rahulraushan137138@gmail.com>
2025-09-28 12:14:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (78.07%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,11 +1,22 @@
- class Solution {
-        public int[] runningSum(int[] nums) {
-          int n = nums.length;  
-          int [] arr = new int[n];
-          arr[0] = nums[0];
-          for(int i=1;i<n;i++){
-              arr[i] = nums[i] + arr[i-1];
-          }
-          return arr;
-        }
+//                 using extra space -> 0(n)
+//  class Solution {
+//         public int[] runningSum(int[] nums) {
+//           int n = nums.length;  
+//           int [] arr = new int[n];
+//           arr[0] = nums[0];
+//           for(int i=1;i<n;i++){
+//               arr[i] = nums[i] + arr[i-1];
+//           }
+//           return arr;
+//         }
+//     }
+
+
+//   Method 2 without using extra space
+
+class Solution{
+    public int[] runningSum(int[] nums){
+        for(int i=1;i<nums.length;i++) nums[i] += nums[i-1];
+        return nums;
     }
+}
