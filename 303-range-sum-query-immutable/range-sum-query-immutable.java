f472f59f9ba2898924eee105a1f684d7d9c8671b
Rahul Raushan <rahulraushan137138@gmail.com>
2025-09-29 09:01:01 +0530

    Time: 7 ms (100.00%) | Memory: 49.7 MB (31.48%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,17 +1,36 @@
-class NumArray {
-    int [] arr;
+//                               Brute Force Method
+// class NumArray {
+//     int [] arr;
 
-    public NumArray(int[] nums) { // Constructor
-       arr = nums; // shallow copy
+//     public NumArray(int[] nums) { // Constructor
+//        arr = nums; // shallow copy
         
-    }
+//     }
     
-    public int sumRange(int left, int right) {
-        int sum = 0;
-        for(int i=left;i<=right;i++){
-            sum +=arr[i];
-        }
-        return sum;
+//     public int sumRange(int left, int right) {
+//         int sum = 0;
+//         for(int i=left;i<=right;i++){
+//             sum +=arr[i];
+//         }
+//         return sum;
+//     }
+// }
+
+//   Method 2 ->   Optimized method
+
+class NumArray {
+    int[] arr;
+
+    public NumArray(int[] nums) {  // Constructor
+      arr = Arrays.copyOf(nums,nums.length); // deep copy
+      for(int i=1;i<nums.length;i++){
+        arr[i] += arr[i-1];
+      }
+
+    }
+
+    public int sumRange (int left, int right){
+        if(left==0) return arr[right];
+        return arr[right] - arr[left-1];
     }
 }
-
