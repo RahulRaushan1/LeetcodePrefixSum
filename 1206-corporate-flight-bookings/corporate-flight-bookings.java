0df6d848eb147245637a779f853a1bb52eb8e009
Rahul Raushan <rahulraushan137138@gmail.com>
2025-10-08 03:14:44 +0530

    Time: 3 ms (96.52%) | Memory: 60 MB (45.50%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,13 +1,34 @@
+//                     Brute Force Method
+// class Solution { 
+//     public int[] corpFlightBookings(int[][] arr, int n) {
+//         int [] ans = new int[n];
+//         for(int i=0;i<arr.length;i++){
+//             int first = arr[i][0];
+//             int last = arr[i][1];
+//             int seats = arr[i][2];
+//             for(int j= first-1;j<=last-1;j++){
+//                 ans[j] += seats;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+// Method 2
+
 class Solution {
     public int[] corpFlightBookings(int[][] arr, int n) {
-        int [] ans = new int[n];
+        int[] ans = new int[n];
         for(int i=0;i<arr.length;i++){
             int first = arr[i][0];
             int last = arr[i][1];
             int seats = arr[i][2];
-            for(int j= first-1;j<=last-1;j++){
-                ans[j] += seats;
-            }
+            ans[first-1] += seats;
+            if(last<n) ans[last] -= seats;
+        }
+        for(int i=1;i<n;i++){
+            ans[i] += ans[i-1];
         }
         return ans;
     }
